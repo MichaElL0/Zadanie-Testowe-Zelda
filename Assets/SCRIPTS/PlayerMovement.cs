3d0580dbf62fe2a3721aa236a4b57ddd2d5ced0e
mik <54302018+MichaElL0@users.noreply.github.com>
2024-09-02 18:48:25 +0200

    Add dash mechanic

--- Assets/SCRIPTS/PlayerMovement.cs
+++ Assets/SCRIPTS/PlayerMovement.cs
@@ -4,31 +4,67 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed = 10f;
-    Rigidbody2D rigidbody;
-    Vector2 movement;
+	[Header("Movement")]
+	public float movementSpeed = 10f;
+	public bool isMoving = false;
+	Vector2 movement;
+	Rigidbody2D rb;
 
+	
+
+
+	[Header("Dash mechanic")]
+	[SerializeField] private float dashCooldown = 1f;
+	float timeBetweenDash;
+	bool isDashing = false;
+	public float dashForce = 20f;
+	public float dashDuration = 0.1f;
 
 	private void Awake()
 	{
-		rigidbody = GetComponent<Rigidbody2D>();
+		rb = GetComponent<Rigidbody2D>();
 	}
 
-	// Start is called before the first frame update
-	void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-		movement.y = Input.GetAxisRaw("Vertical");
-    }
-
-	private void FixedUpdate()
+	void Update()
 	{
-		rigidbody.MovePosition(rigidbody.position + movement.normalized * movementSpeed * Time.fixedDeltaTime);
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+
+		isMoving = movement.x != 0 || movement.y != 0;
+
+		// Dash mechanic
+		if (timeBetweenDash <= 0 && !isDashing)
+		{
+			if (Input.GetKeyDown(KeyCode.LeftShift) && isMoving)
+			{
+				StartCoroutine(PerformDash());
+				timeBetweenDash = dashCooldown;
+			}
+		}
+		else
+		{
+			timeBetweenDash -= Time.deltaTime;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (!isDashing)
+		{
+			rb.MovePosition(rb.position + movementSpeed * Time.fixedDeltaTime * movement.normalized);
+		}
+	}
+
+	// Coroutine to handle the dash action
+	private IEnumerator PerformDash()
+	{
+		isDashing = true;
+		Vector2 dashDirection = movement.normalized; 
+		rb.velocity = dashDirection * dashForce; 
+
+		yield return new WaitForSeconds(dashDuration); 
+
+		rb.velocity = Vector2.zero;
+		isDashing = false;
 	}
 }
